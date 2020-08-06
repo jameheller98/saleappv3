a843bd9ee8b129a9adfc4076faa00be7e70ab8eb
jameheller98 <1651012211tuan@ou.edu.vn>
2020-08-06 12:09:05 +0700

    create add and search product

--- app/dao.py
+++ app/dao.py
@@ -3,14 +3,51 @@ import json
 import os
 
 
-def read_products(category_id=0):
+def read_categories():
+    with open(os.path.join(app.root_path, "data/categories.json"), encoding="utf-8") as f:
+        return json.load(f)
+
+
+def read_products(category_id=0, keyword=None, from_price=None, to_price=None):
     with open(os.path.join(app.root_path, "data/products.json"), encoding="utf-8") as f:
         products = json.load(f)
 
         if category_id > 0:
             products = [p for p in products if p["category_id"] == category_id]
 
+        if keyword:
+            products = [p for p in products if p["name"].lower().find(keyword.lower()) >= 0]
+
+        if from_price and to_price:
+            products = [p for p in products if p["price"] >= float(from_price) and p["price"] <= float(to_price)]
+
         return products
+
+
+def add_product(name, description, price, images, category_id):
+    products = read_products()
+
+    product = {
+        "id": len(products) + 1,
+        "name": name,
+        "description": description,
+        "price": price,
+        "images": images,
+        "category_id": category_id
+    }
+
+    products.append(product)
+
+    try:
+        with open(os.path.join(app.root_path, "data/products.json"), "w", encoding="utf-8") as f:
+            json.dump(products, f, ensure_ascii=False, indent=4)
+
+            import pdb
+            pdb.set_trace()
+
+            return True
+    except Exception as ex:
+        print(ex)
 
 
 if __name__ == "__main__":
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app, dao
 
 
@@ -9,7 +9,31 @@ def index():
 
 @app.route("/products")
 def list_products():
-    return render_template("products.html", products=dao.read_products())
+    kw = request.args.get("keyword")
+    from_price = request.args.get("from_price")
+    to_price = request.args.get("to_price")
+
+    return render_template("products.html",
+                           products=dao.read_products(keyword=kw, from_price=from_price, to_price=to_price))
+
+
+@app.route("/products/add", methods=["post", "get"])
+def add_or_update_products():
+    err = ""
+    if request.method.lower() == "post":
+        name = request.form.get("name")
+        price = request.form.get("price", 0)
+        images = request.form.get("images")
+        description = request.form.get("description")
+        category_id = request.form.get("category_id", 0)
+        if (dao.add_product(name=name, price=price, images=images,
+                            description=description, category_id=category_id)):
+            return redirect(url_for("list_products"))
+
+
+        err = "Something wrong !!! Please back later!!!"
+
+    return render_template("product-add.html", categories=dao.read_categories(), err=err)
 
 
 @app.route("/products/<int:category_id>")
